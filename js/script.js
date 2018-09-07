13b3bcf924538b1013732dee03a22b76eab46b9c
VladislavZvolinsky <zvolinsky@ukr.net>
2018-09-07 19:11:28 +0300

    adding files

---
+++ js/script.js
@@ -0,0 +1,15 @@
+if(document.querySelector('.login-main')) {
+	document.querySelector('.login-main .submit').onclick = function() {
+		window.location = 'main.html';
+	}
+}
+// if(document.querySelector('.popup-choose-report')) {
+// 	document.querySelector('').onclick = function() {
+// 		document.querySelector('.popup-choose-report').classList.add('active');
+// 		document.querySelector('.popupBg').classList.add('active');
+// 	}
+// 	document.querySelector('.popupBg').onclick = function() {
+// 		document.querySelector('.popup-choose-report').classList.remove('active');
+// 		document.querySelector('.popupBg').classList.remove('active');
+// 	}
+// }
