75e140fec39fb3e4dad86d83299033a036b93224
VladislavZvolinsky <zvolinsky@ukr.net>
2018-09-10 11:31:11 +0300

    main.html

--- js/script.js
+++ js/script.js
@@ -3,13 +3,15 @@ if(document.querySelector('.login-main')) {
 		window.location = 'main.html';
 	}
 }
-// if(document.querySelector('.popup-choose-report')) {
-// 	document.querySelector('').onclick = function() {
-// 		document.querySelector('.popup-choose-report').classList.add('active');
-// 		document.querySelector('.popupBg').classList.add('active');
-// 	}
-// 	document.querySelector('.popupBg').onclick = function() {
-// 		document.querySelector('.popup-choose-report').classList.remove('active');
-// 		document.querySelector('.popupBg').classList.remove('active');
-// 	}
-// }
+
+if(document.querySelector('.popup-choose-report')) {
+	document.querySelector('.general-blocks .reports').onclick = function() {
+		event.preventDefault();
+		document.querySelector('.popup-choose-report').classList.add('active');
+		document.querySelector('.popupBg').classList.add('active');
+	}
+	document.querySelector('.popupBg').onclick = function() {
+		document.querySelector('.popup-choose-report').classList.remove('active');
+		document.querySelector('.popupBg').classList.remove('active');
+	}
+}
