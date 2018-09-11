87c7364c44f4cccc4506c1c7400932e10e0e1acf
VladislavZvolinsky <zvolinsky@ukr.net>
2018-09-11 11:33:35 +0300

    news-admin

--- js/script.js
+++ js/script.js
@@ -15,3 +15,46 @@ if(document.querySelector('.popup-choose-report')) {
 		document.querySelector('.popupBg').classList.remove('active');
 	}
 }
+test = document.querySelectorAll('.content-news .descr p');
+more = document.querySelectorAll('.content-news .descr .view');
+textArr = [];
+for(i=0;i<test.length;i++) {
+	if(test[i].innerText.length > 1000) {
+		textArr.push({
+			short: test[i].innerText.substring(0,1000),
+			full: test[i].innerText,
+			ind: i
+		})	
+		test[i].innerHTML=test[i].innerText.substring(0,1000);
+	}
+	else {
+		more[i].style.display='none';
+	}
+}
+for(i=0;i<more.length;i++) {
+	more[i].onclick = viewMore(i);
+}
+function viewMore(index) {
+	return function() {
+		event.preventDefault();
+		if(!more[index].classList.contains('active')) {
+			more[index].classList.add('active');
+			for(i=0;i<textArr.length;i++) {
+				if(textArr[i].ind == index) {
+				test[index].innerHTML=textArr[i].full;
+					
+				}
+			}
+
+		} else {
+			more[index].classList.remove('active');
+			for(i=0;i<textArr.length;i++) {
+				if(textArr[i].ind == index) {
+				test[index].innerHTML=textArr[i].short;
+					
+				}
+			}
+		}
+	}
+}
+// console.log(textArr[0].length)
